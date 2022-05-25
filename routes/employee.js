1d589a18b3131e85feb4cfd4e70368f0364ba9e4
KomalBWable <wablekomal1998@gmail.com>
2022-05-25 10:50:34 +0530

    first commit

--- routes/employee.js
+++ routes/employee.js
@@ -1,18 +1,39 @@
-const { request } = require("express");
-const express = require("express");
-const router = express.Router();
 
+
+const Hapi = require("@hapi/hapi");
+const Joi = require('@hapi/joi');
+Joi.objectId = require('joi-objectid')(Joi)
+const employee = require("../models/employee");
 // Employee model
-const Employee = require("../models/employee");
+const init = async () => {
 
+const Employee = require("../models/employee");
+const server = Hapi.server({
+  port: 3000,
+  host: 'localhost'
+});
+await server.register({
+  plugin: require('hapi-mongodb'),
+  options: {
+    url: 'mongodb+srv://{YOUR-USERNAME}:{YOUR-PASSWORD}@main.zxsxp.mongodb.net/sample_mflix?retryWrites=true&w=majority',
+    settings: {
+        useUnifiedTopology: true
+    },
+    decorate: true
+  }
+});
 // @route   GET /api/employee/
 // @desc    Get all employee
 // @access  Public
-router.get("/", async (req, res) => {
+server.route({
+  method: 'GET',
+  path: '/api/employee/getlist',
+  handler: (request, h)  => {
+    
   try {
     let parsedObject;
-    if (req.query.passingObject) {
-      parsedObject = JSON.parse(req.query.passingObject);
+    if (request.query.passingObject) {
+      parsedObject = JSON.parse(request.query.passingObject);
     }
     let employee;
     if (parsedObject) {
@@ -27,106 +48,138 @@ router.get("/", async (req, res) => {
     } else {
       employee = await Employee.find({});
     }
-    res.send({ employee });
+    h.send({ employee });
+    return employee;
   } catch (err) {
-    res.status(400).send({ error: err });
+    throw Boom.notFound( err);
   }
-});
+  
+}});
 
 // @route   GET /api/employee/:id
 // @desc    Get a specific employee
 // @access  Public
-router.get("/:id", async (req, res) => {
+server.route({
+  method: 'GET',
+  path:'/:id',
+  handler: (request, h) => {
   try {
-    const employee = await Employee.findById(req.params.id);
-    res.send({ employee });
+    const employee = await Employee.findById(request.params.id);
+    h.send({ employee });
+    return employee;
   } catch (err) {
-    res.status(404).send({ message: "Employee not found!" });
+    throw Boom.notFound({ message: "Employee not found!" });
   }
-});
+  
+}});
 // @route   GET /api/employee/count
 // @desc    Get  employee count
 // @access  Public
-router.get("/count", async (req, res) => {
+server.route({
+  method: 'GET',
+  path:'/count',
+  handler: (request, h) => {
   try {
-    const employee = await Employee.count(req.query.passingObject);
-    res.send({ employee });
+    const employee = await Employee.count(request.query.passingObject);
+    h.send({ employee });
+    return employee;
   } catch (err) {
-    res.status(404).send({ message: "Employee not found!" });
+    throw Boom.notFound({ message: "Employee not found!" });
   }
-});
+  
+}});
 // @route   POST /api/employee/
 // @desc    Create a employee
 // @access  Public
-router.post("/", async (req, res) => {
+server.route({
+  method: ['PUT', 'POST'],
+  path: '/',
+  handler: function (request, h)  {
   try {
     const newEmployee = await Employee.create({
-      firstname: req.body.firstname,
-      lastname: req.body.lastname,
-      email: req.body.email,
-      mobile: req.body.mobile,
-      gender: req.body.gender,
-      designation: req.body.designation,
-      dateofjoining: req.body.dateofjoining,
-      reportingmanager: req.body.reportingmanager,
-      salary: req.body.salary,
-      employeecode: req.body.employeecode,
-      location: req.body.location,
-      state: req.body.state,
-      country: req.body.country,
-      department: req.body.department,
+      firstname: request.body.firstname,
+      lastname: request.body.lastname,
+      email: request.body.email,
+      mobile: request.body.mobile,
+      gender: request.body.gender,
+      designation: request.body.designation,
+      dateofjoining: request.body.dateofjoining,
+      reportingmanager: request.body.reportingmanager,
+      salary: request.body.salary,
+      employeecode: request.body.employeecode,
+      location: request.body.location,
+      state: request.body.state,
+      country: request.body.country,
+      department: request.body.department,
       deletedAt: null,
     });
-    res.send({ newEmployee });
+    h.send({ newEmployee });
+    return employee;
   } catch (err) {
-    res.status(400).send({ error: err });
+    throw Boom.notFound(err);
   }
-});
+  
+}});
 
 // @route   PUT /api/employee/:id
 // @desc    Update a employee
 // @access  Public
-router.put("/:id", async (req, res) => {
+server.route({
+  method: ['PUT', 'POST'],
+  path: '/:id',
+  handler: function (request, h) {
   try {
     const updatedEmployee = await Employee.findByIdAndUpdate(
-      req.params.id,
-      req.body
+      request.params.id,
+      request.body
     );
-    res.send({ message: "The employee was updated" });
+    h.send({ message: "The employee was updated" });
   } catch (err) {
-    res.status(400).send({ error: err });
+    console.log(err)
+    throw Boom.notFound(err);
   }
-});
+  return employee;
+}});
 
 // @route   DELETE /api/employee/:id
 // @desc    Delete a employee
 // @access  Public
-router.delete("/:id", async (req, res) => {
+server.route({
+  method: 'DELETE',
+  path:'/:id',
+  handler: (request, h) => {
   try {
-    const foundedEmployee = await Employee.findById(req.params.id);
+    const foundedEmployee = await Employee.findById(request.params.id);
     foundedEmployee["deletedAt"] = new Date();
     const removeEmployee = await Employee.findByIdAndUpdate(
-      req.params.id,
+      request.params.id,
       foundedEmployee
     );
-    res.send({ message: "The employee was removed" });
+    h.send({ message: "The employee was removed" });
   } catch (err) {
-    res.status(400).send({ error: err });
+    throw Boom.notFound( err );
   }
-});
+  return employee;
+}});
 
-router.delete("/activate/:id", async (req, res) => {
+server.route({
+  method: 'DELETE',
+  path:'/activate/:id',
+  handler: (request, h) => {
   try {
-    const foundedEmployee = await Employee.findById(req.params.id);
+    const foundedEmployee = await Employee.findById(request.params.id);
     foundedEmployee["deletedAt"] = null;
     const removeEmployee = await Employee.findByIdAndUpdate(
-      req.params.id,
+      request.params.id,
       foundedEmployee
     );
-    res.send({ message: "The employee was removed" });
+    h.send({ message: "The employee was removed" });
   } catch (err) {
-    res.status(400).send({ error: err });
+    throw Boom.notFound(err );
   }
-});
+  return employee;
+}});
+await server.start();
 
-module.exports = router;
+};
+init();
--- server.js
+++ server.js
@@ -1,25 +1,50 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const path = require("path");
+"use strict";
+const Hapi = require("@hapi/hapi");
+const Path = require("path");
 require("./models/db");
-const app = express();
+// const server = Hapi.server({ port: 3000 });
+// server.state("*", {
+//   ttl: null,
+//   isSecure: true,
+//   isHttpOnly: true,
+// });
 
-const PORT = process.env.PORT || 5000;
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(require("cors")());
-app.use(require("helmet")());
-app.use("/api/employee", require("./routes/employee"));
+// app.use(require("cors")());
+// app.use(require("helmet")());
+// app.use("/api/employee", require("./routes/employee"));
 
 // Production
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendfile(path.resolve(__dirname, "client", "build", "index.html"));
+// if (process.env.NODE_ENV === "production")
+//   // Set static folder
+//   app.use(express.static("client/build"));
+const init = async () => {
+  const server = new Hapi.Server({
+    port: 3000,
+    host: "localhost",
+    routes: {
+      files: {
+        relativeTo: Path.join(__dirname, "public"),
+      },
+    },
   });
-}
 
-app.listen(PORT, () => console.log(`App running on port ${PORT}`));
+  await server.register(require("@hapi/inert"));
+  server.route({
+    method: "GET",
+    path: "/",
+    handler: (request, h) => {
+      console.log("h");
+      // return h.file("path");
+      return "heloooooooo";
+    },
+  });
+  await server.start();
+  console.log("Server running on port 3000");
+};
+process.on("unhandledRejection", (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+// .listen(PORT, () => console.log(`App running on port ${PORT}`)app);
+init();
